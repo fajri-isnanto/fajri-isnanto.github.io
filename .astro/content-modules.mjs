db87791fc14ed372110b2bd7938ec96bc10a3618
fajri-isnanto <fajriisnanto1011@gmail.com>
2026-07-22 23:39:06 +0700

    delete ignored folder

--- .astro/content-modules.mjs
+++
@@ -1,6 +0,0 @@
-
-export default new Map([
-["src/content/posts/example-mdx-article.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fexample-mdx-article.mdx&astroContentModuleFlag=true")],
-["src/content/projects/finance-app.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Ffinance-app.mdx&astroContentModuleFlag=true")],
-["src/content/projects/pro-accountant.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fpro-accountant.mdx&astroContentModuleFlag=true")]]);
-		
